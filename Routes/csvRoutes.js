9351731361a84660d4d098231a11373f1ccc61b1
huzaifa <huzaifa@kali.kali>
2024-04-16 02:40:40 +0500

    CORS Issue Fixed

--- Routes/csvRoutes.js
+++ Routes/csvRoutes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const router = express.Router();
 const csvController = require('../Controllers/csvController');
 const multer = require('multer');
+const path = require('path'); // Node.js module for handling file paths
 
 // Set up multer for handling file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    // Ensure the 'uploads' directory exists
+    cb(null, path.join(__dirname, '../uploads/')); // Using path.join for cross-platform compatibility
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -18,7 +20,7 @@ const upload = multer({ storage: storage });
 
 // Error handling middleware
 const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.message);
+  console.error('Error:', err);
   res.status(500).json({ error: 'Internal server error' });
 };
 
