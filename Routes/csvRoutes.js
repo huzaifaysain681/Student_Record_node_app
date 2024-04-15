134439b06f270ee6a5ae63700985cf1d0e05c264
huzaifa <huzaifa@kali.kali>
2024-04-16 02:44:08 +0500

    CORS Issue Fixed

--- Routes/csvRoutes.js
+++ Routes/csvRoutes.js
@@ -3,13 +3,11 @@ const express = require('express');
 const router = express.Router();
 const csvController = require('../Controllers/csvController');
 const multer = require('multer');
-const path = require('path'); // Node.js module for handling file paths
 
 // Set up multer for handling file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Ensure the 'uploads' directory exists
-    cb(null, path.join(__dirname, '../uploads/')); // Using path.join for cross-platform compatibility
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -23,6 +21,7 @@ const errorHandler = (err, req, res, next) => {
   console.error('Error:', err);
   res.status(500).json({ error: 'Internal server error' });
 };
+
 
 // Route for importing CSV file
 router.post('/import', upload.single('csvFile'), csvController.importCSV);
